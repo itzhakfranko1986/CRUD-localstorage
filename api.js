32c91d2d89279fccb02c2960c6108b35e05ee391
Joey <joeyclapton42@gmail.com>
2019-12-09 07:06:50 -0300

    MASTER
    Aplicação

---
+++ api.js
@@ -0,0 +1,62 @@
+(() => {
+  const apiKey = 'b0e16dacb2be405388bb6a2d84d1d09d';
+
+  const menu = document.querySelector('select');
+  const sectionNews = document.querySelector('.section-news');
+
+  menu.innerHTML = renderOptions();
+
+  menu.addEventListener('change', () => {
+    const query = menu.options[menu.selectedIndex].value;
+    sectionNews.innerHTML = '';
+    configRequest(query);
+  });
+
+  function configRequest(query) {
+    const url = `https://newsapi.org/v2/everything?q=${query}&sortBy=publishedAt&language=pt&apiKey=${apiKey}`;
+
+    fetch(url)
+      .then(res => {
+        res
+          .text()
+          .then(result =>
+            JSON.parse(result).articles.map(
+              news => (sectionNews.innerHTML += renderNews(news))
+            )
+          );
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  function renderOptions() {
+    const categories = JSON.parse(window.localStorage.getItem('categories'));
+
+    return categories.map(
+      category => `
+        <option value="${category.category}">${category.menuName}</option>
+      `
+    );
+  }
+
+  function renderNews(news) {
+    return ` <article class="card" id="article">
+
+    <img class="card__image" src="${
+      news.urlToImage
+        ? news.urlToImage
+        : 'https://www.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg'
+    }"/>
+    <div>
+      <h2 class="card__title">${news.title}</h2>
+      <p class="card__description">${
+        news.description ? news.description : ''
+      }</p>
+    </div>
+    <p class="card__author"> ${news.author ? news.author : 'Desconhecido'}</p>
+    </article>
+    `;
+  }
+  renderOptions();
+})();
---
+++ main.js
@@ -0,0 +1,108 @@
+(() => {
+  const inputMenu = document.querySelector('[data-form="name-menu"]');
+  const inputCategory = document.querySelector('[data-form="category"]');
+  const tableBody = document.querySelector('.tbody');
+  const form = document.querySelector('.form');
+  const btnEdit = document.querySelector('.form__btn-edit');
+  let currentDB;
+  let categories = JSON.parse(window.localStorage.getItem('categories'))
+    ? JSON.parse(window.localStorage.getItem('categories'))
+    : [];
+
+  showCategories();
+  form.addEventListener('submit', handleSubmit);
+
+  btnEdit.addEventListener('click', editItem);
+  function createCategory({ id, menuName, category }) {
+    return { id, menuName, category };
+  }
+
+  function clearInputs() {
+    inputMenu.value = '';
+    inputCategory.value = '';
+  }
+
+  function generateID() {
+    return (
+      '_' +
+      Math.random()
+        .toString(36)
+        .substr(2, 9)
+    );
+  }
+
+  function generateListHTML() {
+    if (categories) {
+      return categories.map(
+        menu =>
+          `
+          <tr class="table-row">
+            <td><span>${menu.menuName}</span></td>
+            <td><span>${menu.category}</span></td>
+            <td>
+              <button onclick="onRemoveCategory(event)" class="btn-remove"><span id="${menu.id}" class="fas fa-trash"></span>
+              </button>
+            </td>
+            <td>
+              <button onclick="onEditCategory(event)" class="btn-edit">
+                <span data-id="${menu.id}" class="fas fa-pen-square"></span>
+              </button>
+            </td>
+          </tr>
+      `
+      );
+    }
+  }
+
+  function showCategories() {
+    tableBody ? (tableBody.innerHTML = generateListHTML()) : null;
+  }
+
+  function handleSubmit() {
+    event.preventDefault();
+
+    categories.push(
+      createCategory({
+        id: generateID(),
+        menuName: inputMenu.value,
+        category: inputCategory.value
+      })
+    );
+    setItemLS(categories);
+    showCategories();
+    clearInputs();
+  }
+
+  function setItemLS(db) {
+    window.localStorage.setItem('categories', JSON.stringify(db));
+  }
+
+  function editItem() {
+    console.log(currentDB);
+    categories.find(menu => menu.id === currentDB[0].id).menuName =
+      inputMenu.value;
+    categories.find(menu => menu.id === currentDB[0].id).category =
+      inputCategory.value;
+    setItemLS(categories);
+    showCategories();
+  }
+
+  window.onRemoveCategory = e => {
+    const currentItems = categories.filter(menu => menu.id !== e.target.id);
+    window.localStorage.removeItem('categories');
+
+    setItemLS(currentItems);
+
+    categories = JSON.parse(window.localStorage.getItem('categories'));
+    showCategories();
+  };
+
+  window.onEditCategory = e => {
+    const currentItem = categories.filter(
+      menu => menu.id === e.target.dataset.id
+    );
+    inputCategory.value = currentItem[0].category;
+    inputMenu.value = currentItem[0].menuName;
+    return (currentDB = currentItem);
+  };
+})();
