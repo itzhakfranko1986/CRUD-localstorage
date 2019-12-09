5f42513574bc93ab38f9548ff6e410e6074c732d
Joey <joeyclapton42@gmail.com>
2019-12-09 20:47:20 -0300

    MASTER
    Feat: ajusta detalhes

--- api.js
+++ api.js
@@ -1,62 +1,64 @@
 (() => {
-    const apiKey = "b0e16dacb2be405388bb6a2d84d1d09d";
+  const apiKey = 'b0e16dacb2be405388bb6a2d84d1d09d';
 
-    const menu = document.querySelector("select");
-    const sectionNews = document.querySelector(".section-news");
+  const menu = document.querySelector('select');
+  const sectionNews = document.querySelector('.section-news');
 
-    menu.innerHTML = renderOptions();
+  menu.innerHTML = renderOptions();
 
-    menu.addEventListener("change", () => {
-        const query = menu.options[menu.selectedIndex].value;
-        sectionNews.innerHTML = "";
-        configRequest(query);
-    });
+  configRequest(menu[0].value);
 
-    function configRequest(query) {
-        const url = `https://newsapi.org/v2/everything?q=${query}&sortBy=publishedAt&language=pt&apiKey=${apiKey}`;
+  menu.addEventListener('change', () => {
+    const query = menu.options[menu.selectedIndex].value;
+    sectionNews.innerHTML = '';
+    configRequest(query);
+  });
 
-        fetch(url)
-            .then((res) => {
-                res.text().then((result) =>
-                    JSON.parse(result).articles.map(
-                        (news) => (sectionNews.innerHTML += renderNews(news))
-                    )
-                );
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    }
+  function configRequest(query) {
+    const url = `https://newsapi.org/v2/everything?q=${query}&sortBy=publishedAt&language=pt&apiKey=${apiKey}`;
 
-    function renderOptions() {
-        const categories = JSON.parse(
-            window.localStorage.getItem("categories")
-        );
+    fetch(url)
+      .then(res => {
+        res
+          .text()
+          .then(result =>
+            JSON.parse(result).articles.map(
+              news => (sectionNews.innerHTML += renderNews(news))
+            )
+          );
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 
-        return categories.map(
-            (category) => `
+  function renderOptions() {
+    const categories = JSON.parse(window.localStorage.getItem('categories'));
+
+    return categories.map(
+      category => `
         <option value="${category.category}">${category.menuName}</option>
       `
-        );
-    }
+    );
+  }
 
-    function renderNews(news) {
-        return ` <article class="card" id="article">
+  function renderNews(news) {
+    return ` <article class="card" id="article">
 
     <img class="card__image" src="${
-        news.urlToImage
-            ? news.urlToImage
-            : "https://www.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg"
+      news.urlToImage
+        ? news.urlToImage
+        : 'https://www.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg'
     }"/>
     <div>
       <h2 class="card__title">${news.title}</h2>
       <p class="card__description">${
-          news.description ? news.description : ""
+        news.description ? news.description : ''
       }</p>
     </div>
-    <p class="card__author"> ${news.author ? news.author : "Desconhecido"}</p>
+    <p class="card__author"> ${news.author ? news.author : 'Desconhecido'}</p>
     </article>
     `;
-    }
-    renderOptions();
+  }
+  renderOptions();
 })();
